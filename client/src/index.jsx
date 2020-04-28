1aca3407c77943a238c0637a93f180cd35fac8d6
maxwellrk <maxkatz23@gmail.com>
2020-04-28 19:36:19 -0400

    Moved the async conversion function to app.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,21 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import FileInput from './components/FileInput.jsx';
+import Papa from 'papaparse';
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      transactions: [],
+    };
+    this.asyncParse = this.asyncParse.bind(this);
   }
   // handleClick -> send file to the server
+
+  asyncParse(file) {
+    new Promise(function (complete, error) {
+      Papa.parse(file, { complete, error, header: true });
+    }).then((fileToJson) => {
+      //needs to send an axios request to the server(post) with
+      //fileToJson.data
+      console.log(fileToJson.data);
+    });
+  }
 
   render() {
     return (
       <div>
         <h3>The Only Budgeting App</h3>
         <h3>INSERT FILE HERE</h3>
-        <FileInput />
-        {/* <input type="file"></input> */}
+        <FileInput asyncParse={this.asyncParse} />
       </div>
     );
   }
