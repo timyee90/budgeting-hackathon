ca35527157f631c4ead8ff9488688057428ce192
Joseph Oliver Comia <oliver.r.comia@gmail.com>
2020-04-29 14:11:06 -0400

    Implement styling

---
+++ client/src/components/visualization.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Visaulization extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div>This is the Visaulization page</div>;
+  }
+}
+
+export default Visaulization;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import FileInput from './components/FileInput.jsx';
-import Papa from 'papaparse';
-import axios from 'axios';
+import React from "react";
+import ReactDOM from "react-dom";
+import Papa from "papaparse";
+import axios from "axios";
+import FileInput from "./components/FileInput.jsx";
+import Visualization from "./components/Visualization.jsx";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       transactions: [],
+      showChart: false,
     };
     this.asyncParse = this.asyncParse.bind(this);
   }
@@ -21,22 +23,38 @@ class App extends React.Component {
       .then((fileToJson) => {
         //needs to send an axios request to the server(post) with
         //fileToJson.data
-        return axios.post('api/transactions', fileToJson.data);
+        return axios.post("api/transactions", fileToJson.data);
       })
       .then((resp) => {
         console.log(resp);
+        this.setState({
+          showChart: !this.state.showChart,
+        });
+      })
+      .catch((err) => {
+        console.log("Error in processing file: ", err);
       });
   }
 
   render() {
     return (
       <div>
-        <h3>The Only Budgeting App</h3>
-        <h3>INSERT FILE HERE</h3>
-        <FileInput asyncParse={this.asyncParse} />
+        {this.state.showChart ? (
+          <Visualization />
+        ) : (
+          <section id="banner">
+            <div className="inner">
+              <header>
+                <h1>Having trouble with your Finances?</h1>
+                <h2>Let us Help.</h2>
+                <FileInput asyncParse={this.asyncParse} />
+              </header>
+            </div>
+          </section>
+        )}
       </div>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
