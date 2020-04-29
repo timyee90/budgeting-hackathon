c9a9546e97246101c94f28f0850bd54b0f4a222b
Joseph Oliver Comia <oliver.r.comia@gmail.com>
2020-04-29 16:25:42 -0400

    Create table structure

---
+++ client/src/components/TransactionList.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import TransactionItem from "./TransactionItem.jsx";
+
+class TransactionList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        This is where to display the table
+        <div className="txn">
+          <h3>Transactions</h3>
+          <div className="txn-table">
+            <div className="txn-header txn-row">
+              <div className="txn-data">Date</div>
+              <div className="txn-data">Description</div>
+              <div className="txn-data">Amount</div>
+              <div className="txn-data">Category</div>
+              <div className="txn-data">Transaction</div>
+              <div className="txn-data">Account Name</div>
+            </div>
+            {this.props.transactions.map((transaction, i) => {
+              return <TransactionItem transaction={transaction} key={i} />;
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TransactionList;
