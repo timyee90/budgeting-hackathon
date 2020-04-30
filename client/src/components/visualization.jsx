bf3c7e234e7d16126b7b400cc35d07a48f728633
Joseph Oliver Comia <oliver.r.comia@gmail.com>
2020-04-30 13:16:57 -0400

    Add HeaderVisual component

--- client/src/components/visualization.jsx
+++ client/src/components/visualization.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import TransactionList from './TransactionList.jsx';
-import Chart1 from './chart1.jsx';
-import Chart2 from './chart2.jsx';
-import Chart3 from './chart3.jsx';
+import React from "react";
+import TransactionList from "./TransactionList.jsx";
+import HeaderVisual from "./HeaderVisual.jsx";
+import Chart1 from "./chart1.jsx";
+import Chart2 from "./chart2.jsx";
+import Chart3 from "./chart3.jsx";
 class Visualization extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      navigation: 'all',
+      navigation: "all",
     };
     this.handleNavClick = this.handleNavClick.bind(this);
   }
@@ -21,22 +22,23 @@ class Visualization extends React.Component {
     });
   }
 
+  // {/* <Chart1 transactions={this.props.transactions} /> */}
+  // {/* <Chart2 transactions={this.props.transactions} /> */}
+  // {/* <Chart3 transactions={this.props.transactions} /> */}
+
   render() {
-{/* <Chart1 transactions={this.props.transactions} /> */}
-{/* <Chart2 transactions={this.props.transactions} /> */}
-{/* <Chart3 transactions={this.props.transactions} /> */}
     let current;
-    if (this.state.navigation === 'all') {
+    if (this.state.navigation === "all") {
       current = <TransactionList transactions={this.props.transactions} />;
-    } else if (this.state.navigation === 'cat') {
+    } else if (this.state.navigation === "cat") {
       current = <div>Category</div>;
-    } else if (this.state.navigation === 'type') {
+    } else if (this.state.navigation === "type") {
       current = <div>Transaction Type</div>;
     }
     return (
       <div>
         <div className="container level">
-          <div className="title">This is the Visualization page</div>
+          <HeaderVisual transactions={this.props.transactions} />
         </div>
         <nav className="navbar level-left">
           <div className="">
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Papa from 'papaparse';
-import axios from 'axios';
-import FileInput from './components/FileInput.jsx';
-import Visualization from './components/Visualization.jsx';
+import React from "react";
+import ReactDOM from "react-dom";
+import Papa from "papaparse";
+import axios from "axios";
+import FileInput from "./components/FileInput.jsx";
+import Visualization from "./components/Visualization.jsx";
 
 class App extends React.Component {
   constructor() {
@@ -19,7 +19,7 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    return axios.get('api/transactions').then((result) => {
+    return axios.get("api/transactions").then((result) => {
       this.setState({
         transactions: result.data,
       });
@@ -33,10 +33,10 @@ class App extends React.Component {
       .then((fileToJson) => {
         //needs to send an axios request to the server(post) with
         //fileToJson.data
-        return axios.post('api/transactions', fileToJson.data);
+        return axios.post("api/transactions", fileToJson.data);
       })
       .then((resp) => {
-        return axios.get('api/transactions');
+        return axios.get("api/transactions");
       })
       .then((resp) => {
         this.setState(
@@ -46,13 +46,13 @@ class App extends React.Component {
           }
         );
       })
-      .catch((err) => console.log('Error in processing file: ', err));
+      .catch((err) => console.log("Error in processing file: ", err));
   }
 
   //create a filter function change state based on selected filter
   filterTransactionsAndUpdate(tableField, searchValue) {
     axios
-      .get('api/transactions/field', {
+      .get("api/transactions/field", {
         params: {
           tableField,
           searchValue,
@@ -85,4 +85,4 @@ class App extends React.Component {
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
